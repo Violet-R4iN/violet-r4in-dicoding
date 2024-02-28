6b7826584a4041e3a46e1617d6e6ad250631a364
R4iN_Ver <frostscarlet123@gmail.com>
2024-02-28 03:03:29 +0000

    update the app

--- Hello.py
+++ Hello.py
@@ -1,51 +1,95 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import subprocess
+import sys
 
+def install_pkg(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+install_pkg("numpy")
+install_pkg("pandas")
+install_pkg("matplotlib")
+install_pkg("seaborn")
+install_pkg("scipy")
+install_pkg("streamlit")
+install_pkg("babel")
+
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import streamlit as st
-from streamlit.logger import get_logger
 
-LOGGER = get_logger(__name__)
+customers_df = pd.read_csv("day.csv")
 
+customers_df.rename(columns={
+    "cnt": "jumlah_user_total",
+    "atemp": "suhu_relatif"
+}, inplace=True)
 
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
+byworkday_df = customers_df.groupby(by="workingday").agg({
+    "jumlah_user_total"   : "sum",
+})
 
-    st.write("# Welcome to Streamlit! 👋")
+st.header("Hello, i will present my analysis on chart bellow")
 
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+st.subheader("Number of Customer by working day")
+st.write("berdasarkan grafik dibawah bisa dilihat bahwa lebih banyak orang yang menggunakan sepeda pada saat hari kerja")
+plt.figure(figsize=(10, 5))
+colors = ["#D3D3D3", "#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+sns.barplot(
+    y="jumlah_user_total", 
+    x="workingday",
+    data=byworkday_df,
+    palette=colors
+)
+plt.title("Number of Customer by working day", loc="center", fontsize=15)
+plt.ylabel(None)
+plt.xlabel("1 = workday, 0 = not workday")
+plt.tick_params(axis='x', labelsize=12)
+ 
+st.pyplot(plt)
 
 
-if __name__ == "__main__":
-    run()
+
+st.subheader("Number of Customer by season")
+st.write("jumlah user paling tinggi tercatat pada musim fall yang dilanjutkan dengan musim summer dan winter.")
+
+byseason_df = customers_df.groupby(by="season").agg({
+    "jumlah_user_total"   : "sum",
+})
+ 
+plt.figure(figsize=(10, 5))
+colors = ["#D3D3D3", "#D3D3D3", "#72BCD4", "#D3D3D3", "#D3D3D3"]
+sns.barplot(
+    y="jumlah_user_total", 
+    x="season",
+    data=byseason_df.sort_values(by="jumlah_user_total", ascending=False),
+    palette=colors
+)
+plt.title("Number of Customer by seasons", loc="center", fontsize=15)
+plt.ylabel(None)
+plt.xlabel("season (1:springer, 2:summer, 3:fall, 4:winter)")
+plt.tick_params(axis='x', labelsize=12)
+st.pyplot(plt)
+
+st.subheader("Number of Customer by weather")
+st.write("jumlah user paling tinggi bila cuaca cerah , dan berdasarkan grafik semakin parah cuaca semakin menurun jumlah pengguna, sampai pada cuaca extrim (4) sama sekali tidak ada user")
+
+byweather_df = customers_df.groupby(by="weathersit").agg({
+    "jumlah_user_total"   : "sum",
+})
+ 
+plt.figure(figsize=(10, 5))
+colors = ["#72BCD4", "#D3D3D3",  "#D3D3D3", "#D3D3D3"]
+sns.barplot(
+    y="jumlah_user_total", 
+    x="weathersit",
+    data=byweather_df.sort_values(by="jumlah_user_total", ascending=False),
+    palette=colors
+)
+plt.title("Number of Customer by weather", loc="center", fontsize=15)
+plt.ylabel(None)
+plt.xlabel(None)
+plt.tick_params(axis='x', labelsize=12)
+st.pyplot(plt)
